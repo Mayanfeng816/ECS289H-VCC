d49e2d26137026d84d0799a5fab090ab4dacf197
Zhuoli Huang <2915831851@qq.com>
2025-11-26 03:17:03 +0800

    add run multiple times code

---
+++ batch_run_adaptive_vcc.py
@@ -0,0 +1,27 @@
+import os
+
+
+name_file = "imagenet_names.txt"
+
+
+with open(name_file, "r") as f:
+    # names = [line.strip() for line in f.readlines() if line.strip()]
+
+    #现在是1-10，需要11-20改readlines()[10:20]
+    names = [line.strip() for line in f.readlines()[:10] if line.strip()]
+
+# 逐个运行
+for cls in names:
+    output_dir = f"outputs/VCC_adaptive/{cls}"
+    if os.path.exists(output_dir):
+        print(f"⏭  Skip {cls} (already exists)")
+        continue
+    print(f"\n==============================")
+    print(f" Running VCC for class: {cls}")
+    print(f"==============================\n")
+
+    #运行代码改这里
+    cmd = f'python run_adaptive_vcc.py --target_class "{cls}" --adaptive_k'
+    os.system(cmd)
+
+print("\n🎉 All classes processed!")
